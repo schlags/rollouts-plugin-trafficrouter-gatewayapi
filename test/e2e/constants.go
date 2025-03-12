123edcabd43aaace39af9357c1a1e0654af5ff20
Dylan Schlager <dylan.schlager@lattice.com>
2025-03-12 12:05:56 -0600

    lint

--- test/e2e/constants.go
+++ test/e2e/constants.go
@@ -33,8 +33,7 @@ const (
 
 	FIRST_CANARY_ROUTE_WEIGHT      = 0
 	LAST_CANARY_ROUTE_WEIGHT       = 30
-	// HTTPRoute rules that are managed by the rollout should never update their weight to the setWeight value. It should stay as the default 1
-	MANAGED_HTTP_ROUTE_RULE_WEIGHT = 1 
+	MANAGED_HTTP_ROUTE_RULE_WEIGHT = 1 // HTTPRoute rules that are managed by the rollout should never update their weight to the setWeight value. It should stay as the default 1
 
 	RESOURCES_MAP_KEY contextKey = "resourcesMap"
 
