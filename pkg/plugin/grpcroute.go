ab250a48106bccce520abae32c664ccebc335486
Dylan Schlager <62440614+schlags@users.noreply.github.com>
2025-04-07 16:56:30 -0600

    fix(grpcroute): use correct function in removal of grpc header route (#5)
    surprising that no tests caught this (or anything for that matter) for
    the time that this copy/paste mistake was live... for 8 months.

--- pkg/plugin/grpcroute.go
+++ pkg/plugin/grpcroute.go
@@ -144,7 +144,7 @@ func (r *RpcPlugin) setGRPCHeaderRoute(rollout *v1alpha1.Rollout, headerRouting 
 				Name: headerRouting.Name,
 			},
 		}
-		return r.removeHTTPManagedRoutes(managedRouteList, gatewayAPIConfig)
+		return r.removeGRPCManagedRoutes(managedRouteList, gatewayAPIConfig)
 	}
 	ctx := context.TODO()
 	grpcRouteClient := r.GRPCRouteClient
